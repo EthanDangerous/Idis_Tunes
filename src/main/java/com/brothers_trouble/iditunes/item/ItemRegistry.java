6a06ccbe910a9eda7692a4dacfc27b9e5874f705
brothers_trouble <ethan.baraf@gmail.com>
2025-05-01 06:53:17 -0700

    some changes

--- src/main/java/com/brothers_trouble/iditunes/item/ItemRegistry.java
+++ src/main/java/com/brothers_trouble/iditunes/item/ItemRegistry.java
@@ -3,6 +3,7 @@ package com.brothers_trouble.iditunes.item;
 import com.brothers_trouble.iditunes.IdisTunes;
 import com.brothers_trouble.iditunes.sound.SoundRegistry;
 import net.minecraft.world.item.Item;
+import net.minecraft.world.item.Rarity;
 import net.neoforged.bus.api.IEventBus;
 import net.neoforged.neoforge.registries.DeferredHolder;
 import net.neoforged.neoforge.registries.DeferredRegister;
@@ -11,7 +12,7 @@ public class ItemRegistry {
     public static final DeferredRegister.Items ITEMS = DeferredRegister.createItems(IdisTunes.MODID);
 
     public static final DeferredHolder<Item, Item> FINITE_GEMSTONE_MUSIC_DISC = ITEMS.register("finite_gemstone_music_disc",
-            () -> new Item(new Item.Properties().jukeboxPlayable(SoundRegistry.FINITE_GEMSTONE_KEY).stacksTo(1)));
+            () -> new Item(new Item.Properties().jukeboxPlayable(SoundRegistry.FINITE_GEMSTONE_KEY).stacksTo(1).rarity(Rarity.RARE)));
 
     public static void register(IEventBus eventBus) {
         ITEMS.register(eventBus);
